77e464a2f8add7e2dc9439a540d99ad3b6f05bb8
Ibrahim Amin <ibrahim.s.amin@gmail.com>
2020-09-12 12:32:59 +1000

    header componenet completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './Header';
 
 function App() {
-  return <div className='App'>Hellow world</div>;
+  return (
+     <BrowserRouter>
+        <div className='App'>
+           <div className='app'>
+              <Switch>
+                 <Route path='/checkout'>
+                    <h1> checkout</h1>
+                 </Route>
+                 <Route path='/login'>
+                    <h1> login</h1>
+                 </Route>
+                 <Route path='/'>
+                    <Header />
+                    <h1> Home Page</h1>
+                 </Route>
+              </Switch>
+           </div>
+        </div>
+     </BrowserRouter>
+  );
 }
 
 export default App;
