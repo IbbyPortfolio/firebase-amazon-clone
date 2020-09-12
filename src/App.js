a6017697e257b8fff4a1377686ec34e027578607
Ibrahim Amin <ibrahim.s.amin@gmail.com>
2020-09-12 12:56:05 +1000

    banner completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
                  </Route>
                  <Route path='/'>
                     <Header />
-                    <h1> Home Page</h1>
+                    <Home />
                  </Route>
               </Switch>
            </div>
