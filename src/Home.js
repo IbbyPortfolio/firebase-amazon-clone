56934efd30ab9a25aaaaf94affe13aed4b020824
Ibrahim Amin <ibrahim.s.amin@gmail.com>
2020-09-12 14:33:37 +1000

    product component completed

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
    return (
@@ -9,6 +10,54 @@ function Home() {
             src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
             alt=''
          />
+         <div className='home__row'>
+            <Product
+               id='1231231234'
+               title='Product 1'
+               price={11.96}
+               rating={3}
+               image='https://images-na.ssl-images-amazon.com/images/I/41W9YdrRslL._SX348_BO1,204,203,200_.jpg'
+            />
+            <Product
+               id='1231231234'
+               title='Product 1'
+               price={11.96}
+               rating={3}
+               image='https://images-na.ssl-images-amazon.com/images/I/41W9YdrRslL._SX348_BO1,204,203,200_.jpg'
+            />
+         </div>
+         <div className='home__row'>
+            <Product
+               id='1231231234'
+               title='Product 1'
+               price={11.96}
+               rating={3}
+               image='https://images-na.ssl-images-amazon.com/images/I/41W9YdrRslL._SX348_BO1,204,203,200_.jpg'
+            />
+            <Product
+               id='1231231234'
+               title='Product 1'
+               price={11.96}
+               rating={3}
+               image='https://images-na.ssl-images-amazon.com/images/I/41W9YdrRslL._SX348_BO1,204,203,200_.jpg'
+            />
+            <Product
+               id='1231231234'
+               title='Product 1'
+               price={11.96}
+               rating={3}
+               image='https://images-na.ssl-images-amazon.com/images/I/41W9YdrRslL._SX348_BO1,204,203,200_.jpg'
+            />
+         </div>
+         <div className='home__row'>
+            <Product
+               id='1231231234'
+               title='Product 1'
+               price={11.96}
+               rating={3}
+               image='https://images-na.ssl-images-amazon.com/images/I/41W9YdrRslL._SX348_BO1,204,203,200_.jpg'
+            />
+         </div>
       </div>
    );
 }
